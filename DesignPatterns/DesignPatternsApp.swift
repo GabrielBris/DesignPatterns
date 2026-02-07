6f5424ee966349873c18ebb98e36d0fbd4a4cf03
Gabriel Briseño <gabriel_bris1315@outlook.com>
2026-02-07 01:11:10 -0600

    Added project base.

---
+++ DesignPatterns/DesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsApp.swift
+//  DesignPatterns
+//
+//  Created by Gabriel Alejandro Briseño Alvarez on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
